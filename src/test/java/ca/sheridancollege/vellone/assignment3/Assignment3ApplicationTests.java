024d619df44723b976eb63d1ff0d666d18202805
vellone20 <vellone@sheridancollege.ca>
2021-03-26 14:40:35 -0400

    Initial commit

---
+++ src/test/java/ca/sheridancollege/vellone/assignment3/Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.vellone.assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
